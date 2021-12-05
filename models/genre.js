474a69339476c95627a0bb6cc469a881be01c0ab
Claudi0-V <claudiov.dejesus@protonmail.com>
2021-12-05 16:50:20 -0300

    completed the schemes and models

--- models/genre.js
+++ models/genre.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const GenreSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 100
+    }
+})
+
+GenreSchema.virtual('url')
+    .get(() => `/catalog/${this.type}`)
+
+module.exports = mongoose.model('Genre', GenreSchema);
